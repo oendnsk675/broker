d75dab71ddcbb6cdd389cda96d97f3e3cf8b312b
sayidina ahmadal qososyi <58449456+oendnsk675@users.noreply.github.com>
2021-10-13 06:17:44 +0800

    A

--- broker.js
+++ broker.js
@@ -1,6 +1,6 @@
 const aedes = require('aedes')()
 const server = require('net').createServer(aedes.handle)
-const port = 1882
+const port = 5000
 
 server.listen(port, function () {
   console.log('server started and listening on port ', port)
