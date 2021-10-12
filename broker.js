b2e081ec1e66894a9cd60eaf04fe4fe0d6140b87
sayidina ahmadal qososyi <58449456+oendnsk675@users.noreply.github.com>
2021-10-13 06:42:15 +0800

    a

--- broker.js
+++ broker.js
@@ -1,7 +1,26 @@
 const aedes = require('aedes')()
 const server = require('net').createServer(aedes.handle)
-const port = 5000
+const port = 1882
+'use strict';
+
+const { networkInterfaces } = require('os');
+
+const nets = networkInterfaces();
+const results = Object.create(null); // Or just '{}', an empty object
+
+for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        if (net.family === 'IPv4' && !net.internal) {
+            if (!results[name]) {
+                results[name] = [];
+            }
+            results[name].push(net.address);
+        }
+    }
+}
 
 server.listen(port, function () {
+  console.log('server started and listening on ip ', results)
   console.log('server started and listening on port ', port)
 })
--- pub.js
+++ pub.js
@@ -1,6 +1,6 @@
 var mqtt = require('mqtt');
 // var client = mqtt.connect('mqtt://localhost:1882');
-var client = mqtt.connect('mqtt://belajarnode1.herokuapp.com:1882');
+var client = mqtt.connect('mqtt://belajarnode1.herokuapp.com:5000');
 // console.log();
 
 // function randomString(length, chars) {
